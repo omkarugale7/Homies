593ce21e1d6c4cbf1f52d563d9406252f7d5301e
omkar ugale <omkarugale057@gmail,com>
2023-03-16 15:02:52 +0530

    added Storage file

--- ContractStorage/contracts/Storage.sol
+++ ContractStorage/contracts/Storage.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Storage {
-    
+    struct Transaction {
+        string Sender;
+        string receiver;
+        uint256 amount;
+        string date;
+        string transactionId;
+    }
+    Transaction[] public TransactionData;
+
+    function AddtoArray (string memory _sender, string memory _receiver, uint256 _amount, string memory _date, string memory _transactionId) public {
+        TransactionData.push(Transaction(  _sender , _receiver, _amount,  _date,   _transactionId));
+    }
+    function retrieve() public view returns (Transaction [] memory){
+        return TransactionData;
+    }
 }
